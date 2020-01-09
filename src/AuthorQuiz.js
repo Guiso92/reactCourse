7727c9ac16fcc4db478ca1518733b5a6e1bbe075
unknown <emilioguisolfo@gmail.com>
2020-01-09 00:57:29 -0300

    events part of the course

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,11 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import './bootstrap.min.css'
+import './bootstrap.min.css';
+import PropTypes from 'prop-types';
 
-function AuthorQuiz() {
+function Hero() {
   return (
-    <div>Author Quiz</div>
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  )
+}
+
+function Book({title, onClick}) {
+  return(
+    <div className="answer" onClick={() => {onClick(title);}}>
+      <h4>{title}</h4>
+    </div>
+  )
+}
+
+function Turn({author, books, highlight, onAnswerSelected}) {
+ function highlightToBgColor(highlight) {
+   const mapping = {
+     'none': '',
+     'correct': 'green',
+     'wrong': 'red'
+   }
+
+   return mapping[highlight]
+ }
+
+  return (
+    <div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+      <div className="col-md-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="author"/>
+      </div>
+      <div className="col-6">
+        {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+      </div>
+    </div>
+  )
+}
+
+Turn.prototype = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  highlight: PropTypes.string.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired
+}
+
+function Continue() {
+  return (<div />)
+}
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Portada">Wikimedia Commons</a> and are in the public domain</p>
+      </div>
+    </div>
+  )
+}
+
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+      <Continue />
+      <Footer />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,71 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import { shuffle, sample } from 'underscore'
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charlesDickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Charles Dickens']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephConrad.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Joseph Conrad']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenKing.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Stephen King']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/williamShakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of William Shakespeare']
+    }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
+    }
+}
+
+const state = {
+    turnData: getTurnData(authors),
+    highlight: ''
+}
+
+function onAnswerSelected(answer) {
+    const isCorrect = state.turnData.author.books.some((book) => book === answer);
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+
+    render();
+}
+
+function render () {
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
+render();
+
 serviceWorker.unregister();
